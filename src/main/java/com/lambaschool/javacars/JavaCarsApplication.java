43d66e5acd5b85e1edfb4ffddf4be7c7c069d25d
Pablo Ogata <pabloogata@gmail.com>
2019-03-01 11:17:17 -0500

    pabloogata

---
+++ src/main/java/com/lambaschool/javacars/JavaCarsApplication.java
@@ -0,0 +1,13 @@
+package com.lambaschool.javacars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCarsApplication.class, args);
+    }
+
+}
